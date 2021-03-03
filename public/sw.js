98debc9dd68a143f15c81bad73217f79589fdbdf
nzambello <nzambello@protonmail.com>
2021-03-03 22:37:16 +0100

    build: updated build

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e54690f5371edffaa591.js"
+    "url": "webpack-runtime-74ea91eec26980e21596.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "06f97901c3b3c64a1ed39018669e0f6f"
+    "revision": "3d3cf14e4aa6ba2eeae7becbcaa06f01"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
